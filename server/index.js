a06440e9ef7af4e080827f32e57a6ea71f5b428c
iamkervyntan <kervyntan@gmail.com>
2022-12-16 19:51:19 +0800

    added test code for server to listen on port specified in config.env

--- server/index.js
+++ server/index.js
@@ -6,3 +6,7 @@ const app = express();
 dotenv.config({path:'./config/config.env'});
 
 app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port : ${process.env.PORT}`);
+})
