6b9806e122d8333a963aea4d220c2a3b5ef56482
iamkervyntan <kervyntan@gmail.com>
2022-12-16 19:53:13 +0800

    added comment for app.listen

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ dotenv.config({path:'./config/config.env'});
 
 app.use(express.json());
 
+// takes PORT number from config.env file
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening on port : ${process.env.PORT}`);
 })
