b550fdc1b5e13fa3c7240ff480d898968f155469
iamkervyntan <kervyntan@gmail.com>
2022-12-16 21:18:13 +0800

    updated index.js with imports and requires, set up app to use api

--- server/index.js
+++ server/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 const app = express();
+const route = require('./routes/userRoute');
 
 dotenv.config({path:'./config/config.env'});
+require('./config/conn');
 
 app.use(express.json());
+// Using routes
+app.use('/api', route);
+
 
 // takes PORT number from config.env file
 app.listen(process.env.PORT, () => {
