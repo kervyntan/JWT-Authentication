d665641cb237b9ce08092e699ed2a1a32f986223
iamkervyntan <kervyntan@gmail.com>
2022-12-16 19:47:50 +0800

    installed mongoose and dotenv, inserted code for config

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express();
-
 const dotenv = require('dotenv');
 
+const app = express();
+
 dotenv.config({path:'./config/config.env'});
+
+app.use(express.json());
