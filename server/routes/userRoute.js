9c1958f206a76d6385b31b07b40b83c1ea6d7397
iamkervyntan <kervyntan@gmail.com>
2022-12-16 20:08:34 +0800

    creating user routes for register, login and retrieving user's data

---
+++ server/routes/userRoute.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const route = express.Router();
+// import userModel
+const userModel = require("../models/userModel");
+
+// User register route:
+route.post("/register", (req, res) => {
+
+})
+
+// User login route:
+route.post("/login", (req, res) => {
+
+})
+
+// GET user's data
+route.get('/user', (req, res) => {
+    
+})
